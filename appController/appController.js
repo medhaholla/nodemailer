373a74a4fc6ec6e2e20e7dd5a652d0eacc29d091
medhaholla <holla.medha@gmail.com>
2023-12-20 14:51:17 +0530

    Update email configuration for Gmail service

--- appController/appController.js
+++ appController/appController.js
@@ -1,4 +1,20 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+
 const sendEmail = (req, res) => {
+  const { userEmail } = req.body;
+
+  let confif = {
+    service: "gmail",
+    auth: {
+      user: email,
+      pass: password,
+    },
+  };
+
   res.status(201).json("GetEmail sucessful");
 };
 
